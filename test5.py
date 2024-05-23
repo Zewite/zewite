d0369a8e7654f1c25ee064d5e6a8c745517842f7
Zewite <167963802+Zewite@users.noreply.github.com>
2024-05-23 12:51:26 +0300

    Add files via upload

---
+++ test5.py
@@ -0,0 +1,74 @@
+'''''is_rainy = True # will be rain
+if is_rainy:
+    print('take umbrella')
+    print('put on your boots')
+else:
+    print('I dont take an umbrella')
+print('going for a walk')''''''
+'''''''
+is_rainy = False # dont will be rain
+if is_rainy:
+    print('take umbrella')
+    print('put on your boots')
+else:
+    print('I dont take an umbrella')
+print('going for a walk')
+'''''
+'''''''''
+is_rainy = True
+heavy_rain = False
+if is_rainy:
+    if heavy_rain:
+        print('I take umbrella')
+    else:
+        print('I take raincoat')
+else:
+    print('I dont take an umbrella')
+print('going for a walk')
+''''''''
+print('five is more than three?', 5 > 3)
+'''
+'''''''''
+lis = [1,2,3,4,5]
+print('in list have number seven?', 7 in lis)
+'''
+#stv = 'str with number 1'
+# sub = 'str' in stv
+#print('In str have word Str?', sub)
+#number = 12
+#cond1 = number % 2 == 0
+#cond2 = number % 3 ==0
+#if cond1 and cond2:
+ #   print('Number == 2 and 3')
+#else:
+ #   print('Number dont == 2 and 3')
+#mount = 12
+#bad = \
+#     mount == 12 or \
+#     mount == 1 or \
+#     mount == 2
+# if bad:
+#      print('Winter')
+# good = mount in [12,1,2]
+# if good:
+#     print('Winter!!!')
+# hour = 7
+# good = 6 <= hour < 12
+# if good:
+#     print("Morning!!!")
+# lis = [5,6,7,8,9]
+# result = (4 in lis) and (8 in lis)
+# print(result)
+# month = int(input('Enter a number:'))
+# if month in [1,2,12]:
+#     print('Winter')
+# elif month in [3,4,5]:
+#     print('Spring')
+# elif month in [6,7,8]:
+#     print('Summer')
+# elif month in [9,10,11]:
+#     print('Autumn')
+# else:
+#     print('you dude lol hahahahahahh')
+login_in = True
+items_in_cart = Trueshipping
